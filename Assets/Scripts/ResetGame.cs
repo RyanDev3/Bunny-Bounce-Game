d423e052bacc5d229b7cc6993cae89211b71b998
Ryan <121602177+RyanDev2@users.noreply.github.com>
2025-03-11 13:46:24 +0000

    Reset Button (No Death I Swear)

---
+++ Assets/Scripts/ResetGame.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleReset : MonoBehaviour
+{
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
