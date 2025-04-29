f803667efae1e25d17a33b50f8c6c26b319e7c7d
Ryan <ryanmcgorie@gmail.com>
2025-04-29 13:39:54 +0100

    ResetGame Fix

--- Assets/Scripts/ResetGame.cs
+++ Assets/Scripts/ResetGame.cs
@@ -3,12 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class SimpleReset : MonoBehaviour
 {
+    [Tooltip("Choose which scene to load when resetting")]
+    public string targetScene = "Tutorial"; // Default
+
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene("Tutorial");
+            if (!string.IsNullOrEmpty(targetScene))
+                SceneManager.LoadScene(targetScene);
+            else
+                Debug.LogError("No scene selected for reset!");
         }
     }
 }
