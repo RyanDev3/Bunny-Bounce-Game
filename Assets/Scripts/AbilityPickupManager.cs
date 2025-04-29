d2ef657330805ae34164c67df4df579581cd592b
Ryan <ryanmcgorie@gmail.com>
2025-04-29 16:03:41 +0100

    end screen no ui

--- Assets/Scripts/AbilityPickupManager.cs
+++ Assets/Scripts/AbilityPickupManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AbilityPickupManager : MonoBehaviour
 {
@@ -17,5 +18,9 @@ public class AbilityPickupManager : MonoBehaviour
     {
         CurrentAbilityLevel += v;
         OnAbilityLevelIncrease?.Invoke(CurrentAbilityLevel);
+    }
+    public void GameEnd()
+    {
+        SceneManager.LoadScene("EndScreen");
     }
 }
